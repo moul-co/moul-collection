7b3a26a9dd2b38888130a414cd9691d8ee2bb82c
Sophearak Tha <t.sophearak@gmail.com>
2018-12-26 21:04:01 +0700

    initial release

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import MoulCollection from './index.vue';
+
+new Vue({
+  render: h => h(MoulCollection)
+}).$mount('#photos');
---
+++ src/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="pswp__bg"></div>
+      <div class="pswp__scroll-wrap">
+        <div class="pswp__container">
+          <div class="pswp__item"></div>
+          <div class="pswp__item"></div>
+          <div class="pswp__item"></div>
+        </div>
+        <div class="pswp__ui pswp__ui--hidden">
+          <div class="pswp__top-bar">
+            <div class="pswp__counter"></div>
+            <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+            <button class="pswp__button pswp__button--share" title="Share"></button>
+            <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+            <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+            <div class="pswp__preloader">
+              <div class="pswp__preloader__icn">
+                <div class="pswp__preloader__cut">
+                  <div class="pswp__preloader__donut"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+            <div class="pswp__share-tooltip"></div>
+          </div>
+          <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+          <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+          <div class="pswp__caption">
+            <div class="pswp__caption__center"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="collection" :style="{ width: `${containerW}px`, height: `${containerH}px` }">
+      <figure v-for="(photo, key) in collection" :key="key">
+        <a :href="`/${photo.srcHd}`" :data-dimension="photo.dimension">
+          <img :src="`/${photo.src}`" :alt="photo.name" :style="photo.inline">
+        </a>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+import fixedPartition from './layouts/fixed-partition.js';
+import { photoswipe } from './layouts/photoswipe.js';
+
+export default {
+  name: 'MoulCollection',
+  data() {
+    return {
+      photos: [],
+      currentWidth: window.innerWidth,
+      containerW: 0,
+      containerH: 0
+    };
+  },
+  computed: {
+    collection() {
+      let idealHeight = 320;
+
+      if (this.currentWidth < 600) {
+        idealHeight = 200;
+      }
+      const photo = fixedPartition(this.photos, {
+        containerWidth: this.currentWidth - 16,
+        idealElementHeight: idealHeight,
+        spacing: 8
+      });
+
+      const elements = [],
+        positions = photo.positions;
+
+      positions.forEach((position, index) => {
+        elements.push({
+          name: this.photos[index].name,
+          srcHd: this.photos[index].srcHd,
+          dimension: `${this.photos[index].widthHd}x${
+            this.photos[index].heightHd
+          }`,
+          src: this.photos[index].src,
+          inline: {
+            width: `${positions[index].width}px`,
+            height: `${positions[index].height}px`,
+            top: `${positions[index].y}px`,
+            left: `${positions[index].x}px`,
+            backgroundImage: `url(data:image/svg+xml;base64,${
+              this.photos[index].sqip
+            }`
+          }
+        });
+      });
+
+      // eslint-disable-next-line
+      this.containerW = photo.width;
+      // eslint-disable-next-line
+      this.containerH = photo.height;
+
+      return elements;
+    }
+  },
+  methods: {
+    handleResize() {
+      this.currentWidth = window.innerWidth;
+    }
+  },
+  beforeMount() {
+    let photoCollection = document
+      .querySelector('#photo-collection')
+      .getAttribute('value');
+    this.photos = JSON.parse(photoCollection);
+  },
+  mounted() {
+    (() => {
+      const throttle = (type, name, obj) => {
+        obj = obj || window;
+        let running = false;
+        const func = () => {
+          if (running) {
+            return;
+          }
+          running = true;
+          requestAnimationFrame(() => {
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+          });
+        };
+        obj.addEventListener(type, func);
+      };
+
+      throttle('resize', 'optimizedResize');
+    })();
+    window.addEventListener('optimizedResize', this.handleResize);
+    photoswipe('.collection');
+  }
+};
+</script>
